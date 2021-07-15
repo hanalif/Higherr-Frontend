8b6c1a00473c52027f69ec217342c71722b9afb4
Hana Lipschutz <hanalif619@gmail.com>
2021-07-15 09:24:57 +0300

    style

--- src/pages/user-details.vue
+++ src/pages/user-details.vue
@@ -1,17 +1,29 @@
 <template>
-  <section class="main-layout">
+  <section class="user-details-page main-layout">
     <div class="user-details">
-    <div class="profile-details">
+    <div class="info-column user-details-card">
        <div class="online-tag">online</div>
-      <div class="top-details"> 
-      <img class="profile-img" src="https://cdn1.iconfinder.com/data/icons/avatar-97/32/avatar-02-512.png" alt="">
+      <div class="top-details">
+        <div class="user-profile-img">
+          <img class="profile-img" src="https://cdn1.iconfinder.com/data/icons/avatar-97/32/avatar-02-512.png" alt="">
+          </div> 
       <h2 class="profile-name-title">hana</h2>
+      <div class="user-stats">
+        <div class="user-stat-line">
+          <span> <i class="fas fa-map-marker-alt"></i> from</span>
+          <span>Israel</span>
+        </div>
+        <div class="user-stat-line">
+          <span> <i class="fas fa-user"></i> Member since</span>
+          <span>2020</span>
+        </div>
+      </div>
       </div>
       <div class="buttom-details">
 
       </div>
     </div>
-    <div class="gigs-container">
+    <div class="gigs-column user-details-card">
       <div class="user-details-bar">
         <button>create new gig</button>
       </div>
