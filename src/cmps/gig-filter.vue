62c9f7c20fc5b9fa0a6138378b1a6aab0a821b61
ben itzhak <benitzhak72@gmail.com>
2021-07-15 13:26:51 +0300

    commit

--- src/cmps/gig-filter.vue
+++ src/cmps/gig-filter.vue
@@ -1,26 +1,54 @@
 <template>
 <section class="filter">
-    <input type="text" placeholder="Search By Title.."/>
-    <select name="" id="">
+    <input type="text" v-model="filterBy.txt" placeholder="Search By Title.."/>
+    <select name="" id="" v-model="filterBy.tags">
         <option value="media">Media</option>
         <option value="web-develop">Web-Develop</option>
         <option value="grafic">Grafic</option>
         <option value="comunication">Comunication</option>
+        <option value="all">All</option>
     </select>
-    <select name="" id="">
+    <select name="" id="" v-model="filterBy.delivery">
         <option value="1">Up To 1 Day</option>
         <option value="3">Up To 3 Days</option>
         <option value="7">Up To 7 Days</option>
         <option value="all">All</option>
     </select>
-    <input type="number" placeholder="Min Price">
-    <input type="number" placeholder="Max Price">
-    <button>save</button>
+    <input type="number" placeholder="Min Price"  v-model="filterBy.price.min">
+    <input type="number" placeholder="Max Price"  v-model="filterBy.price.max">
+    <button @click="filter">save</button>
 </section>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            filterBy:{
+                txt:'',
+                tags:'all',
+                delivery: 'all',
+                price:{
+                    min:0,
+                    max:Infinity
+                    }
+            }
+        }
+    },
+    methods:{
+        filter(){
+            const filterBy = {
+                txt:this.filterBy.txt,
+                tags:this.filterBy.tags,
+                delivery: this.filterBy.delivery,
+                price:{
+                    min:this.filterBy.price.min,
+                    max:this.filterBy.price.max
+                }
+            }
+            this.$store.dispatch({type:'setFilter',filterBy})
+        }
+    }
 
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
-import app from './App.vue'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/style.scss'
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/swiper-bundle.css'
 
+Vue.use(VueAwesomeSwiper)
 
 
 new Vue({
-  router,
-  store,
-  render: h => h(app)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
-
-
-
- 
