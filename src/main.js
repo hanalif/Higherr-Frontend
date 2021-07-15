89aaf407f4b1f327d6babb41609886447c5c4e3e
Hana Lipschutz <hanalif619@gmail.com>
2021-07-15 11:37:25 +0300

    user details style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import app from './App.vue'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/style.scss'
@@ -9,7 +9,7 @@ import './styles/style.scss'
 new Vue({
   router,
   store,
-  render: h => h(app)
+  render: h => h(App)
 }).$mount('#app')
 
 
--- src/pages/gig-details.vue
+++ src/pages/gig-details.vue
@@ -10,18 +10,10 @@
     </nav>
     <div class="gig-overview-contanier" v-if="gig">
       <div class="gig-info">
-<<<<<<< HEAD
-        <!-- <gig-overview id="gig-overview" :gig="gig" /> -->
-        {{gig}}
-        <!-- <gig-description id="gig-description" />
-        <about-seller id="about-seller" />
-        <gig-review id="gig-review" /> -->
-=======
         <gig-overview id="gig-overview" :gig="gig" />
         <gig-description id="gig-description" :gig="gig" />
         <about-seller id="about-seller" :gig="gig" />
         <gig-review id="gig-review" />
->>>>>>> b043cdb5d111b4443e334ba6e0695f729598ea29
       </div>
       <!-- <order-details /> -->
     </div>
@@ -35,15 +27,6 @@ import aboutSeller from "../cmps/about-seller.vue";
 import gigReview from "../cmps/gig-review.vue";
 import orderDetails from "../cmps/order-details.vue";
 export default {
-<<<<<<< HEAD
-  computed: {
-    gig() {
-      return this.$store.getters.gigToShow
-      },
-  },
-  created(){
-    this.$store.dispatch({ type: "getGigById", id: this.$route.params.gigId,})
-=======
     data() {
         return {
             gig: null
@@ -53,7 +36,6 @@ export default {
        this.$store.dispatch({ type: "getGigById", id: this.$route.params.gigId})
        .then(gig => this.gig = gig)
 
->>>>>>> b043cdb5d111b4443e334ba6e0695f729598ea29
   },
   components: {
     gigOverview,
