4a9f9db6b6a96df5feea925147d67bf5434e0330
Hana Lipschutz <hanalif619@gmail.com>
2021-07-17 11:30:39 +0300

    user modal open

--- src/cmps/edit-user-form-modal.vue
+++ src/cmps/edit-user-form-modal.vue
@@ -1,28 +1,19 @@
 <template>
   <div class="user-edit-modal">
-    <modal @close="close" ref="modal">
+    <modal @close="close">
       <template v-slot:header>
         <h1>Edit User Details</h1>
       </template>
 
       <template v-slot:body>
         <div class="form-body">
-          <input
-            type="text"
-            v-model="userCred.username"
-            placeholder="Username"
-          />
-          <input
-            type="password"
-            v-model="userCred.password"
-            placeholder="Password"
-          />
+          <input type="text" v-model="user.from" placeholder="Username" />
         </div>
       </template>
 
       <template v-slot:footer>
-        <button class="btn" @click="closeModal">Cancel</button>
-        <button class="btn-primary btn" @click="loginSubmit">Save</button>
+        <button class="btn" @click="close">Cancel</button>
+        <button class="btn-primary btn" @click="formSubmit">Save</button>
       </template>
     </modal>
   </div>
@@ -36,17 +27,14 @@ export default {
       user: {
         from: "",
         description: "",
-        skills: ""
+        skills: "",
       },
     };
   },
   methods: {
+    formSubmit() {},
     close() {
       this.$emit("close");
-    },
-    loginSubmit() {
-      this.$store.dispatch({ type: "login", userCred: this.userCred });
-      this.close();
     },
   },
   created() {},
