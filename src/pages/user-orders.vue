b17758a25b9c05216429c58610bfdb36982e3582
Hana Lipschutz <hanalif619@gmail.com>
2021-07-23 10:28:54 +0300

    order confirmation modal

--- src/pages/user-orders.vue
+++ src/pages/user-orders.vue
@@ -2,38 +2,7 @@
   <div class="user-orders-page">
     <div class="user-orders main-layout">
       <h1 class="user-orders-main-title main-title">{{this.user.fullname +`'s`}} orders</h1>
-      
-      <div class="user-orders-as-seller-container">
-      <table>
-        <caption class="user-orders-main-title">
-          My orders 
-        </caption>
-        <thead>
-          <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Status</th>
-            <th scope="col">Created at</th>
-            <th scope="col">Price</th>
-            <th scope="col">Seller</th>
-            <th scope="col">Contact seller</th>
-            <th scope="col">Remove order</th>
-          </tr>
-        </thead>
-        <template v-if="userAsBuyerOrders"> 
-          <tbody  v-for="order in userAsBuyerOrders" :key="order._id">
-          <tr>
-            <td scope="row" data-label="Title">{{order.title}}</td>
-            <td data-label="Status">{{order.status}}</td>
-            <td data-label="Created at">{{order.createdAt}}</td>
-            <td data-label="Price"><span>$</span>{{order.price}}</td>
-            <td data-label="Seller">{{order.seller.fullname}}</td>
-            <td data-label="Contact seller"><button class="btn">Contact seller</button></td>
-            <td data-label="Remove order"><i @click="onRemoveOrder(order._id)" class="fas fa-trash"></i></td>
-          </tr>
-        </tbody> 
-        </template>
-      </table>
-      </div>
+
       <div class="user-orders-as-buyer-container">
       <table>
         <caption class="user-orders-main-title">
@@ -52,7 +21,7 @@
         <template v-if="userAsSellerOrders">
         <tbody v-for="order in userAsSellerOrders" :key="order._id">
           <tr>
-            <td scope="row" data-label="Title">{{order.title}}</td>
+            <td scope="row" data-label="Title" class="table-order-title">{{order.title}}</td>
             <td data-label="Status">{{order.status}}</td>
             <td data-label="Created at">{{order.createdAt}}</td>
             <td data-label="Price"><span>$</span>{{order.price}}</td>
@@ -60,6 +29,38 @@
             <td data-label="Contact seller"><button class="btn">Contact buyer</button></td>
           </tr>
         </tbody>
+        </template>
+      </table>
+      </div>
+
+      <div class="user-orders-as-seller-container">
+      <table>
+        <caption class="user-orders-main-title">
+          My orders 
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Status</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Price</th>
+            <th scope="col">Seller</th>
+            <th scope="col">Contact seller</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <template v-if="userAsBuyerOrders"> 
+          <tbody  v-for="order in userAsBuyerOrders" :key="order._id">
+          <tr>
+            <td scope="row" data-label="Title" class="table-order-title">{{order.title}}</td>
+            <td data-label="Status">{{order.status}}</td>
+            <td data-label="Created at">{{order.createdAt}}</td>
+            <td data-label="Price"><span>$</span>{{order.price}}</td>
+            <td data-label="Seller">{{order.seller.fullname}}</td>
+            <td data-label="Contact seller"><button class="btn">Contact seller</button></td>
+            <td data-label="Remove order" ><button @click="onRemoveOrder(order._id)" class="btn btn-primary">Cancel order</button></td>
+          </tr>
+        </tbody> 
         </template>
       </table>
       </div>
