45a8b9d8e3d9bc7454829f5235e5dce3569922d4
ben itzhak <benitzhak72@gmail.com>
2021-07-25 11:47:49 +0300

    commit

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -47,13 +47,13 @@
       <div class="about-contianer grid">
         <div class="about-content">
           <h2>A whole world of freelance talent at your fingertips</h2>
-          <h3>✅ The best for every budget</h3>
+          <h3>✔ The best for every budget</h3>
           <p>Find high-quality services at every price point. No hourly rates, just project-based pricing.</p>
-          <h3>✅ Quality work done quickly</h3>
+          <h3>✔ Quality work done quickly</h3>
           <p>Find the right freelancer to begin working on your project within minutes.</p>
-          <h3>✅ Protected payments, every time</h3>
+          <h3>✔ Protected payments, every time</h3>
           <p>Always know what you'll pay upfront. Your payment isn't released until you approve the work.</p>
-          <h3>✅ 24/7 support</h3>
+          <h3>✔ 24/7 support</h3>
           <p>Questions? Our round-the-clock support team is available to help anytime, anywhere.</p>
         </div>
         <div class="about-img">
