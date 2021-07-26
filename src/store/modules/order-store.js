2c7b9f7c1b926eb70b75fe12d569586501426769
Hana Lipschutz <hanalif619@gmail.com>
2021-07-26 17:02:24 +0300

    console.log

--- src/store/modules/order-store.js
+++ src/store/modules/order-store.js
@@ -20,7 +20,6 @@ export default {
         addOrder(state, { order }) {
             if (!state.orders.some(o => o._id === order._id)) {
                 state.orders.unshift(order)
-                console.log(state.orders)
             }
         },
         updateOrder(state, { order }) {
@@ -29,7 +28,6 @@ export default {
         },
         setOrders(state, { orders }) {
             state.orders = orders
-            console.log(state.orders)
         },
         setLoggedinUserOrders(state, { loggedinUserOrders }) {
             state.loggedInUserOrders = loggedinUserOrders
